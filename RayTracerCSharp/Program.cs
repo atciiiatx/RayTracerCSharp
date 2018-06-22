d8e87ecde9e322f9b083aee26a5c27bfb379a5a8
A. T. Campbell, III <acampbell@hookbang.com>
2018-06-22 17:31:05 -0500

    Add first raytrace

--- RayTracerCSharp/Program.cs
+++ RayTracerCSharp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Drawing;
+using System.Numerics;
 
 namespace RayTracerCSharp
 {
@@ -11,6 +12,7 @@ namespace RayTracerCSharp
     static void Main(string[] args)
     {
       CreateTestPPM();
+      CreateTestRayTrace();
     }
 
 
@@ -29,7 +31,40 @@ namespace RayTracerCSharp
           bmp.SetPixel(ix, iy, Color.FromArgb(r, g, b));
         }
       }
-      bmp.Save("out.png", System.Drawing.Imaging.ImageFormat.Png);
+      bmp.Save("ppmOut.png", System.Drawing.Imaging.ImageFormat.Png);
+    }
+
+    static Vector3 RayColor(Ray r)
+    {
+      Vector3 unitDirection = r.Direction / r.Direction.Length();
+      float t = 0.5f * (unitDirection.Y + 1.0f);
+      return (1.0f - t) * Vector3.One + t * new Vector3(0.5f, 0.7f, 1.0f);
+    }
+
+    static void CreateTestRayTrace()
+    {
+      int nx = 200;
+      int ny = 100;
+      Bitmap bmp = new Bitmap(nx, ny);
+      Vector3 lowerLeftCorner = new Vector3(-2, -1, -1);
+      Vector3 horizontal = new Vector3(4, 0, 0);
+      Vector3 vertical = new Vector3(0, 2, 0);
+      Vector3 origin = Vector3.Zero;
+      for (int iy = ny - 1; iy >= 0; --iy)
+      {
+        for (int ix = 0; ix < nx; ++ix)
+        {
+          float u = (float)ix / (float)nx;
+          float v = (float)iy / (float)ny;
+          Ray ray = new Ray(origin, lowerLeftCorner + u * horizontal + v * vertical);
+          Vector3 rayColor = RayColor(ray);
+          int r = (int)(255.99 * rayColor.X);
+          int g = (int)(255.99 * rayColor.Y);
+          int b = (int)(255.99 * rayColor.Z);
+          bmp.SetPixel(ix, iy, Color.FromArgb(r, g, b));
+        }
+      }
+      bmp.Save("rayTraceOut.png", System.Drawing.Imaging.ImageFormat.Png);
     }
   }
 }
---
+++ RayTracerCSharp/Ray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace RayTracerCSharp
+{
+  class Ray
+  {
+    // Default Constructor
+    public Ray()
+    {
+    }
+
+    // Data constructor
+    public Ray(Vector3 a, Vector3 b)
+    {
+      Origin = a;
+      Direction = b;
+    }
+
+    // Properties
+    public Vector3 Origin { get; set; }
+    public Vector3 Direction { get; set; }
+
+    // Point along ray
+    public Vector3 PointAtParameter(float t)
+    {
+      return Origin + t * Direction;
+    }
+  }
+}
